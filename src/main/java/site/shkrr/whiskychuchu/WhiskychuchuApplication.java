7c1af052c191254977ad09d49b73744a2c037541
SangHun <shzero211@naver.com>
2022-11-19 22:34:00 +0900

    spring.io로 초기 셋팅

---
+++ src/main/java/site/shkrr/whiskychuchu/WhiskychuchuApplication.java
@@ -0,0 +1,13 @@
+package site.shkrr.whiskychuchu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskychuchuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiskychuchuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/shkrr/whiskychuchu/WhiskychuchuApplicationTests.java
@@ -0,0 +1,13 @@
+package site.shkrr.whiskychuchu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiskychuchuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
