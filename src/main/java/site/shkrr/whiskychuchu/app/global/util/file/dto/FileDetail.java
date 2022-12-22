d69de03a90315033bc45a4939425b388ddd52ce7
SangHun <shzero211@naver.com>
2022-12-22 23:52:32 +0900

    feat (S3) #7: S3 이미지를 처리에 필요한 클래스 생성 - AwsS3ResourceService: S3에서 이미지 처리 기능을 가지는 서비스 - MultipartUtil: MultipartFile 과 연관된 기능 처리 -  FileDetail: MultipartFile 에서 가져온 정보를 담는 클래스

---
+++ src/main/java/site/shkrr/whiskychuchu/app/global/util/file/dto/FileDetail.java
@@ -0,0 +1,33 @@
+package site.shkrr.whiskychuchu.app.global.util.file.dto;
+
+import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+import site.shkrr.whiskychuchu.app.global.util.file.MultipartUtil;
+/**
+ *
+ * MultipartFile 에서 S3파일처리에 필요한 정보를가져와 가공하는 클래스
+ * */
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class FileDetail {
+    private String oriImgName;
+    private String uuid;
+    private String extension;
+    private String savedName;
+    private String savedPath;
+
+    public static FileDetail multipartOf(MultipartFile file){
+        String uuid=MultipartUtil.createUUID();
+        String extension=MultipartUtil.getExtension(file.getContentType());
+        return FileDetail.builder()
+                .oriImgName(file.getOriginalFilename())
+                .uuid(uuid)
+                .extension(extension)
+                .savedName(MultipartUtil.createSavedName(uuid,extension))
+                .build();
+    }
+}
